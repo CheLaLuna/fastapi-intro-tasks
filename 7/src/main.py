5e81de4c11542bdc3d963771bff965e33784c099
CheLaLuna <160347343+CheLaLuna@users.noreply.github.com>
2024-12-31 11:34:06 +0300

    Update main.py

--- 7/src/main.py
+++ 7/src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import List
 
-
 app = FastAPI()
 
 # Временная база данных
@@ -10,5 +9,21 @@ product_list = []
 product_id_counter = 1
 
 # BEGIN (write your solution here)
+class Product(BaseModel):
+    name: str
+    price: float = Field(..., gt=0)
+    quantity: int = Field(..., ge=0)
 
+@app.post("/product")
+async def create_product(product: Product):
+    global product_id_counter
+    product_data = product.dict()
+    product_data["id"] = product_id_counter
+    product_list.append(product_data)
+    product_id_counter += 1
+    return {"message": "Product added successfully", "product": product_data}
+
+@app.get("/products", response_model=List[Product])
+async def get_products():
+    return {"products": product_list}
 # END
